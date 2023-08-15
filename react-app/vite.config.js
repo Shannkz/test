5583fbdb58952e34c8f49cd0a374c159cbccd0dd
Dragos Iancu <draio@Dragoss-MacBook-Pro.local>
2023-08-15 19:51:35 +0300

    added gitignore

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,  // Needed for port mapping
+    strictPort: true,
+    port: 5173,  // can be changed to whatever port
+  }
 });
