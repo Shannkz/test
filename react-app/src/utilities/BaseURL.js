326ad20d0073cc10add0f16901b77b77a5f1c47f
Dragos Iancu <draio@192-168-0-100.rdsnet.ro>
2023-08-18 00:45:08 +0300

    quick change to frontend-2

--- react-app/src/utilities/BaseURL.js
+++ react-app/src/utilities/BaseURL.js
@@ -3,7 +3,7 @@ let baseURL = "";
 if (window && window._env_ && window._env_.REACT_APP_GW_URL) {
   baseURL = window._env_.REACT_APP_GW_URL;
 } else {
-  baseURL = "http://127.0.0.1:8000";
+  baseURL = "http://django-test-lb-1263148841.eu-central-1.elb.amazonaws.com:8080";
 }
 export default baseURL;
 
