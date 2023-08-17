fccae28b050460c3eedcd5c8d8616685adc274af
Dragos Iancu <draio@192-168-0-100.rdsnet.ro>
2023-08-18 00:02:18 +0300

    modified Dockerfile

---
+++ backend/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Prepare database migrations
+python manage.py makemigrations
+
+# Migrate the changes to database
+python manage.py migrate
+
+# Create the super user to be used
+python manage.py createsuperuser --noinput --first_name dragos --last_name dragos
+
+# Start the application server
+python manage.py runserver 0.0.0.0:8000
